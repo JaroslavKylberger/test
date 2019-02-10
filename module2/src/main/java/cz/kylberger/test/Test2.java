69ada39744ad1a0a372a5fd7943c3b93089c9e1a
Jaroslav Kylberger <jaroslav.kylberger@gmail.com>
2019-02-10 11:10:32 +0100

    adding module2

---
+++ module2/src/main/java/cz/kylberger/test/Test2.java
@@ -0,0 +1,13 @@
+package cz.kylberger.test;
+
+public class Test2 {
+
+    public static void main(String[] args) {
+        print();
+    }
+
+    static void print() {
+        System.out.println("HaHa2");
+    }
+
+}
---
+++ module2/src/test/java/cz/kylberger/test/Test2Test.java
@@ -0,0 +1,10 @@
+package cz.kylberger.test;
+
+import org.junit.Test;
+
+public class Test2Test {
+    @Test
+    public void test() {
+        Test2.print();
+    }
+}
