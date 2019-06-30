22b09bb84243eff6221e4ae1dad4500fd4d317bc
Jaroslav Kylberger <jaroslav.kylberger@gmail.com>
2019-06-30 22:18:34 +0200

    Java 8 SE Certification sample questions

--- module1/src/main/java/cz/kylberger/test/JavaSETest.java
+++ module1/src/main/java/cz/kylberger/test/JavaSETest.java
@@ -1,12 +1,18 @@
 package cz.kylberger.test;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.*;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 public class JavaSETest {
 
     public static void main(String[] args) {
+        Collection<String> source = Arrays.asList("Cat","Dog","Rat");
+        List<Integer> list = getLengthCollection(source, ArrayList::new);
+        System.out.println(list);
+        Set<Integer> set = getLengthCollection(source, HashSet::new);
+        System.out.println(set);
         List<Integer> elements = new ArrayList<>();
         elements.add(10);
         int firstElmnt = elements.get(0);
@@ -23,5 +29,9 @@ public class JavaSETest {
             e.printStackTrace();
         }
         executor.shutdown();
+    }
+
+    private static <C extends Collection<Integer>> C getLengthCollection(Collection<String> source, Supplier<C> supplier) {
+        return source.stream().map(String::length).collect(Collectors.toCollection(supplier));
     }
 }
